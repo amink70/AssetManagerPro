c3c321bf22e9afe11fe6f6193e06ba797b789a63
Amin Kargar <amin.kargar1370@gmail.com>
2026-07-03 16:24:21 +0330

    Update AssetManagerPro

---
+++ AssetManagerPro/ViewModels/AssetManagementViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using AssetManagerPro.Models;
+using AssetManagerPro.Repositories;
+
+namespace AssetManagerPro.ViewModels;
+
+public class AssetManagementViewModel
+{
+    private readonly AssetRepository _repository = new();
+
+    public ObservableCollection<AssetDisplay> Assets { get; } = new();
+
+    public AssetManagementViewModel()
+    {
+        LoadAssets();
+    }
+
+    public void LoadAssets()
+    {
+        Assets.Clear();
+
+        foreach (var asset in _repository.GetAll())
+        {
+            Assets.Add(asset);
+        }
+    }
+}
